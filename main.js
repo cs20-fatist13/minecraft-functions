9b992db9a34e882220554ca3d6ddb14d2596b3f6
Basaam Anwar <109045924+fatist13@users.noreply.github.com>
2023-09-07 12:40:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -24,68 +24,30 @@ function fishBtnClicked() {
   let character = charSelect.value;
 
   if (character === 'steve') {
-    // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
-    catchFish(0.7, 0.2, 0.05, 0.05);
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  if (character === 'steve') {
-    // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
-    let randNum = Math.random();
-    if (randNum < 0.7) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = 'img/Raw-Cod.png';
-    } else if (randNum < 0.9) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = 'img/Raw-Salmon.png';
-    } else if (randNum < 0.95) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = 'img/Tropical-Fish.png';
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = 'img/Pufferfish.png';
-    }
+    catchFish(0.7, 0.2, 0.05, 0.02);
   } else if (character === 'alex') {
-    // ALEX: Cod 10%, Salmon 10%, Tropical 30%, Puffer 50%
-    let randNum = Math.random();
-    if (randNum < 0.1) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = 'img/Raw-Cod.png';
-    } else if (randNum < 0.2) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = 'img/Raw-Salmon.png';
-    } else if (randNum < 0.5) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = 'img/Tropical-Fish.png';
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = 'img/Pufferfish.png';
-    }
-  } 
+    catchFish(0.1, 0.1, 0.3, 0.5);
+  }
+
 }
 
-function catchFish() {
-let randNum = Math.random();
-numCod++;
-numCodSpan.innerHTML = numCod;
-resultImg.src = 'img/Raw-Cod.png';
-
-
+function catchFish(codProb, salmonProb, tropicalProb, pufferProb) {
+ let randNum = Math.random();
+  if (codProb > randNum) {
+    numCod++;
+    numCodSpan.innerHTML = numCod;
+    resultImg.src = 'img/Raw-Cod.png';
+ } else if (salmonProb < randNum) {
+    numSalmon++;
+    numSalmonSpan.innerHTML = numSalmon;
+  resultImg.src = 'img/Raw-Salmon.png';
+  } else if (tropicalProb > randNum) {
+    numTropical++;
+    numTropicalSpan.innerHTML = numTropical;
+    resultImg.src = 'img/Tropical-Fish.png';
+  } else if (pufferProb > randNum) {
+    numPuffer++;
+    numPufferSpan.innerHTML = numPuffer;
+    resultImg.src = 'img/Pufferfish.png';
+  }
 }
