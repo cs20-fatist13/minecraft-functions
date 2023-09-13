29fec878f8e186667701fef5de786699c9ae1cf2
Basaam Anwar <109045924+fatist13@users.noreply.github.com>
2023-09-12 18:10:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// Functionize Minecraft Fishing Start Code
-
-// Variables to store HTML Elements
-let fishBtn = document.getElementById('fish-btn');
-let charSelect = document.getElementById('character-select');
-let resultImg = document.getElementById('result-img');
-let numCodSpan = document.getElementById('num-cod');
-let numSalmonSpan = document.getElementById('num-salmon');
-let numTropicalSpan = document.getElementById('num-tropical');
-let numPufferSpan = document.getElementById('num-puffer');
+let fishBtn = document.getElementById("fish-btn");
+let charSelect = document.getElementById("character-select");
+let resultImg = document.getElementById("result-img");
+let numCodSpan = document.getElementById("num-cod");
+let numSalmonSpan = document.getElementById("num-salmon");
+let numTropicalSpan = document.getElementById("num-tropical");
+let numPufferSpan = document.getElementById("num-puffer");
 
 // Global Variables
 let numCod = 0;
@@ -16,38 +13,38 @@ let numTropical = 0;
 let numPuffer = 0;
 
 // Add Event Listener to Calculate Button
-fishBtn.addEventListener('click', fishBtnClicked);
+fishBtn.addEventListener("click", fishBtnClicked);
 
 // Event handler for fishBtn
 function fishBtnClicked() {
   // Check Selected Character
   let character = charSelect.value;
 
-  if (character === 'steve') {
+  if (character === "steve") {
     catchFish(0.7, 0.2, 0.05, 0.02);
-  } else if (character === 'alex') {
+  } else if (character === "alex") {
     catchFish(0.1, 0.1, 0.3, 0.5);
   }
-
 }
 
 function catchFish(codProb, salmonProb, tropicalProb, pufferProb) {
- let randNum = Math.random();
+  let randNum = Math.random();
+
   if (codProb > randNum) {
     numCod++;
     numCodSpan.innerHTML = numCod;
-    resultImg.src = 'img/Raw-Cod.png';
- } else if (salmonProb < randNum) {
+    resultImg.src = "img/Raw-Cod.png";
+  } else if (salmonProb > randNum) {
     numSalmon++;
     numSalmonSpan.innerHTML = numSalmon;
-  resultImg.src = 'img/Raw-Salmon.png';
-  } else if (tropicalProb < randNum) {
+    resultImg.src = "img/Raw-Salmon.png";
+  } else if (tropicalProb > randNum) {
     numTropical++;
     numTropicalSpan.innerHTML = numTropical;
-    resultImg.src = 'img/Tropical-Fish.png';
+    resultImg.src = "img/Tropical-Fish.png";
   } else if (pufferProb > randNum) {
     numPuffer++;
     numPufferSpan.innerHTML = numPuffer;
-    resultImg.src = 'img/Pufferfish.png';
+    resultImg.src = "img/Pufferfish.png";
   }
 }
